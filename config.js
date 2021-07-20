45052d6c106a32945610233420b0515a24c26d21
Huynh-Ngoc-Tai <56644659+Huynh-Ngoc-Tai@users.noreply.github.com>
2021-07-20 17:42:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Ơ kìa! Em thích anh rồi à 😙',
+    desc: 'Thích anh thế cơ á ',
+    btnYes: 'Vângg <33',
+    btnNo: 'Không  :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Thích anh rất nhiều <33333333',
+    mess: 'Anh biết mà 🥰. Anh cũng thích em. 😘😘',
+    messDesc: 'inbox cho anh.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //https://m.me/huynhngoctaidz
+    messLink: 'http://facebook.com' //https://m.me/huynhngoctaidz
 }
